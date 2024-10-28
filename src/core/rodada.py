e953e9762d361f95a51182218700acd16b609082
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 19:14:42 -0300

    feat: adding class 'Rodada'

---
+++ src/core/rodada.py
@@ -0,0 +1,21 @@
+class Rodada:
+    def __init__(self, jogador: Jogador, comprou_carta: bool = False):
+        pass
+
+    def comparar_jogador(self, jogador: Jogador) -> bool:
+        pass
+
+    def alterar_comprou_carta(self):
+        pass
+
+    def verificar_compra(self):
+        pass
+
+    def get_jogador(self) -> Jogador:
+        pass
+
+    def adicionar_jogador(jog_contrario: Jogador) -> Jogador:
+        pass
+
+    def set_jogador(self, prox_jogador: Jogador) -> Jogador:
+        pass
