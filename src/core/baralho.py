7eeb09af29615d61aeeede8aded4f14f9dc6bf34
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 19:14:06 -0300

    feat: adding class 'Baralho'

---
+++ src/core/baralho.py
@@ -0,0 +1,10 @@
+class Baralho:
+    def __init__(self, cartas: list[Carta]):
+        pass
+
+    def reiniciar_baralho(self):
+        pass
+
+    def get_cartas(self) -> list[Cartas]:
+        pass
+
