d52892117711d9a5642b2c87d359b919ff39d1a6
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 18:07:43 -0300

    feat: adding class 'Pilha'

---
+++ src/core/pilha.py
@@ -0,0 +1,16 @@
+class Pilha:
+    def __init__(self, cartas, eh_canto):
+        pass
+
+    def verifica_colocacao_carta(self, carta: Carta) -> bool:
+        pass
+
+    def verifica_canto(self) -> bool:
+        pass
+
+    def adicionar_cartas_pilha(self, lista_cartas: list):
+        pass
+
+    def retirar_cartas_pilha(self, lista_cartas: list):
+        pass
+
