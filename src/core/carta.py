0d118935b9b65d483ef5f2bbb681ccf1f27e501e
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 19:14:24 -0300

    feat: adding class 'Carta'

---
+++ src/core/carta.py
@@ -0,0 +1,13 @@
+class Carta:
+    def __init__(self, naipe: Naipe, numero: int):
+        pass
+
+    def verificar_rei(self) -> bool:
+        pass
+
+    def verificar_cor_carta(self) -> str:
+        pass
+
+    def get_numero(self) -> int:
+        pass
+
