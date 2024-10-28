6f1130bf252b8e24f0305fd2146b3588908e5450
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 17:46:45 -0300

    feat: adding class 'Partida'

---
+++ src/core/partida.py
@@ -0,0 +1,64 @@
+class Partida:
+    def __init__(self, jogador_local: Jogador, jogador_remoto: Jogador, mesa: Mesa, rodada_atual: Rodada):
+        pass
+
+    def abandonar_partida():
+        pass
+
+    def obtem_status():
+        pass
+
+    def receive_start(jogadores: list):
+        pass
+
+    def obter_status_partida() -> str:
+        pass
+
+    def restaurar_estado_inicial():
+        pass
+
+    def reiniciar_vitoria():
+        pass
+
+    def passar_a_vez():
+        pass
+
+    def pegar_jog_contrario(jogador: Jogador) -> Jogador:
+        pass
+
+    def desistir():
+        pass
+
+    def comecar_partida(jogadores: list):
+        pass
+
+    def avaliar_vencedor() -> bool:
+        pass
+
+    def mover_cartas():
+        pass
+
+    def jogar_carta():
+        pass
+
+    def comprar_carta():
+        pass
+
+    def colocar_rei():
+        pass
+
+    def receber_jogada():
+        pass
+
+    def verificar_inicio() -> bool:
+        pass
+
+    def obter_jogada(jogada: dict):
+        pass
+
+    def encerrar_partida():
+        pass
+
+    def get_encerrou() -> bool:
+        pass
+
