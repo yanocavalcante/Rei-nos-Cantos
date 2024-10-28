5cd7c9d7dc4ceea69a921caa4486a1d18f80b703
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 18:01:44 -0300

    hotfix: forgot adding 'self' instance referencing parameter

--- src/core/partida.py
+++ src/core/partida.py
@@ -2,63 +2,63 @@ class Partida:
     def __init__(self, jogador_local: Jogador, jogador_remoto: Jogador, mesa: Mesa, rodada_atual: Rodada):
         pass
 
-    def abandonar_partida():
+    def abandonar_partida(self):
         pass
 
-    def obtem_status():
+    def obtem_status(self):
         pass
 
-    def receive_start(jogadores: list):
+    def receive_start(self, jogadores: list):
         pass
 
-    def obter_status_partida() -> str:
+    def obter_status_partida(self) -> str:
         pass
 
-    def restaurar_estado_inicial():
+    def restaurar_estado_inicial(self):
         pass
 
-    def reiniciar_vitoria():
+    def reiniciar_vitoria(self):
         pass
 
-    def passar_a_vez():
+    def passar_a_vez(self):
         pass
 
-    def pegar_jog_contrario(jogador: Jogador) -> Jogador:
+    def pegar_jog_contrario(self, jogador: Jogador) -> Jogador:
         pass
 
-    def desistir():
+    def desistir(self):
         pass
 
-    def comecar_partida(jogadores: list):
+    def comecar_partida(self, jogadores: list):
         pass
 
-    def avaliar_vencedor() -> bool:
+    def avaliar_vencedor(self) -> bool:
         pass
 
-    def mover_cartas():
+    def mover_cartas(self):
         pass
 
-    def jogar_carta():
+    def jogar_carta(self):
         pass
 
-    def comprar_carta():
+    def comprar_carta(self):
         pass
 
-    def colocar_rei():
+    def colocar_rei(self):
         pass
 
-    def receber_jogada():
+    def receber_jogada(self):
         pass
 
-    def verificar_inicio() -> bool:
+    def verificar_inicio(self) -> bool:
         pass
 
-    def obter_jogada(jogada: dict):
+    def obter_jogada(self, jogada: dict):
         pass
 
-    def encerrar_partida():
+    def encerrar_partida(self):
         pass
 
-    def get_encerrou() -> bool:
+    def get_encerrou(self) -> bool:
         pass
 
