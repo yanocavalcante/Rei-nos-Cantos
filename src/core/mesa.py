5780d97d199daef8e4cfec51601815e3bd103503
Yano Henrique de Melo Ribeiro Cavalcante <yano.melo.cavalcante@gmail.com>
2024-10-28 18:55:38 -0300

    feat: adding class 'Mesa'

---
+++ src/core/mesa.py
@@ -0,0 +1,19 @@
+class Mesa:
+    def __init__(self, pilhas: list, monte: Pilha):
+        pass
+
+    def limpar_mesa(self):
+        pass
+
+    def remover_carta_monte(self, carta: Carta):
+        pass
+
+    def comprar_carta_monte(self) -> Carta:
+        pass
+
+    def embaralhar_monte(self, lista_cartas: list) -> list:
+        pass
+
+    def colocar_cartas_mesa(self, lista_cartas: list) -> list:
+        pass
+
