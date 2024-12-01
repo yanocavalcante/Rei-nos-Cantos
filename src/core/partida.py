7fd2be658a962da34ed8397c3c6403fe7119520e
marinabenvenuti <marinabcardeal@gmail.com>
2024-12-01 18:26:45 -0300

    fix receber jogada de jogar carta

--- src/core/partida.py
+++ src/core/partida.py
@@ -223,7 +223,7 @@ class Partida:
             
             elif jogada['tipo_jogada'] == 'rei_no_canto' or jogada['tipo_jogada'] == 'jogar':
                 self._jogador_remoto.remover_carta((self._mesa.get_cartas_codigo(jogada['cartas']))[-1])
-                self._mesa.get_pilha_codigo(jogada['pilha_adiciona']).adicionar_cartas_pilha(self._mesa.get_cartas_codigo([jogada['cartas']]))
+                self._mesa.get_pilha_codigo(jogada['pilha_adiciona']).adicionar_cartas_pilha([self._mesa.get_cartas_codigo([jogada['cartas']])])
             
                 if jogada['venceu'] == 'True':
                     self.set_partida_em_andamento()
